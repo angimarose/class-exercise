7a378d8d8887a1f5ed3f45e08384f9b69b6cede3
angimarose <42789234+angimarose@users.noreply.github.com>
2018-09-27 21:04:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 /* global L */
 
 var mapOptions = {
-  minZoom: 2, 
+  minZoom: 2,
   maxZoom: 18,
-  zoomControl: false, 
+  zoomControl: false,
   attributeControl: false,
 }
 
-var map = L.map('map', mapOptions).setView([8.9806, 38.757], 0);
+var map = L.map('map', mapOptions).setView([20, 40], 0);
 
 // Add base layer
 
 L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
   minZoom: 2,
-  maxZoom: 18, 
-  zoomControl: false, 
+  maxZoom: 18,
+  zoomControl: false,
   attributionControl: false,
 }).addTo(map);
 
@@ -29,13 +29,13 @@ fetch('https://cdn.glitch.com/74f074b0-aac6-45cd-8ad6-7be45ed1840e%2FOrigin_pt.g
   })
 
   .then(function (data) {
-    // Create the Leaflet layer for the data 
+    // Create the Leaflet layer for the data
     var coffeeOrigins = L.geoJson(data, {
-      
+
       pointToLayer: function (geoJsonPoint, latlng) {
         return L.circleMarker(latlng);
       },
-      
+
       // Then we can style them as we would other features
       style: function (geoJsonFeature) {
         return {
@@ -46,17 +46,17 @@ fetch('https://cdn.glitch.com/74f074b0-aac6-45cd-8ad6-7be45ed1840e%2FOrigin_pt.g
       };
      }
     });
-  
-    
+
+
     coffeeOrigins.addTo(map);
     console.log(data)
-  
+
   //style popups
-  
-  
+
+
   //add popups for Origins
     coffeeOrigins.bindPopup(function (layer) {
-      
+
       return '<div class="origin-name">' + layer.feature.properties['origin'];
     });
 });
@@ -64,10 +64,8 @@ fetch('https://cdn.glitch.com/74f074b0-aac6-45cd-8ad6-7be45ed1840e%2FOrigin_pt.g
 var coffeeOrigins=L.geoJson();
 var sliderControl = L.control.sliderControl({position: "bottomleft", layer: coffeeOrigins});
 
-//add the slider to the map 
+//add the slider to the map
 map.addControl(sliderControl);
 
 //And initialize the slider
 sliderControl.startSlider();
-      
-      
